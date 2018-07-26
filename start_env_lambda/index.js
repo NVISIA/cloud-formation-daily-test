00c15c23ddfa65974a5c1fa905deac39fddaaf9f
Paul Knell <paul_knell@hotmail.com>
2018-07-26 11:36:27 -0500

    changes for addition of SMS notification of errors

--- start_env_lambda/index.js
+++ start_env_lambda/index.js
@@ -3,6 +3,9 @@ exports.handler = function(event, context, callback) {
    var AWS = require('aws-sdk');
    var cloudformation = new AWS.CloudFormation();
 
+   var account_id = context.invokedFunctionArn.split(":")[4]
+   var region = process.env.AWS_DEFAULT_REGION
+
    var params = {
      StackName: event.stackName, /* required */
      Capabilities: [
@@ -31,14 +34,27 @@ exports.handler = function(event, context, callback) {
        }
      ],
      TemplateURL: 'https://editions-us-east-1.s3.amazonaws.com/aws/stable/Docker.tmpl',
-     TimeoutInMinutes: 20
+     TimeoutInMinutes: 20,
+     NotificationARNs: [ 'arn:aws:sns:' + region + ':' + account_id + ':cloudformation-events' ]
    };
    cloudformation.createStack(params, function(err, data) {
      if (err) {
-        callback("Error creating the Stack: "+err); 
+       var sns = new AWS.SNS();
+       var topic_arn = "arn:aws:sns:" + region + ":" + account_id + ":error-notification"
+       sns.publish({
+         Subject: 'Error during createStack',
+         Message: 'Error during createStack: ' + err,
+         TopicArn: topic_arn
+       }, function(err, data) {
+         if (err) {
+           console.log(err.stack);
+         }
+       });
+       callback("Error creating the Stack: "+err); 
      }
      else {
-        callback(null, "Success creating the Stack.");
+       callback(null, "Success creating the Stack.");
      }
    });
 }
+
